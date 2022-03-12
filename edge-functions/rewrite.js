12b0fb4789fa3d1afabb4bf38eae491acff63c03
Amos Chen <n.a>
2022-03-12 12:47:34 -0500

    Change path from /old_path/ to /new_path/

--- edge-functions/rewrite.js
+++ edge-functions/rewrite.js
@@ -1,5 +1,24 @@
+/**
+ * Introducing CloudFront Functions – Run Your Code at the Edge with Low Latency at Any Scale
+ * https://aws.amazon.com/blogs/aws/introducing-cloudfront-functions-run-your-code-at-the-edge-with-low-latency-at-any-scale/
+ * 
+ * CAUTION:
+ * Syntax has to be ECMAScript 5.1 compliant. For example, use 'var' instead of 'const'.
+ */
+
 function handler(event) {
-  // Rewrite request URI to serve the same file
-  event.request.uri = '/test_site/test.html';
-  return event.request;
+
+  var request = event.request;
+  var uri     = request.uri;
+  var match1  = '/old_path/';  
+
+  // Use .indexOf() for best performance. https://www.measurethat.net/Benchmarks/Show/4797/1/js-regex-vs-startswith-vs-indexof
+  if (uri.indexOf(match1) === 0) {
+    // substr() is faster than replace(). https://stackoverflow.com/questions/9586705/performance-about-replace-or-substr-in-javascript
+    request.uri = '/new_path/' + uri.substr(match1.length);
+  }
+
+  return request;
 }
+
+module.exports = handler;
